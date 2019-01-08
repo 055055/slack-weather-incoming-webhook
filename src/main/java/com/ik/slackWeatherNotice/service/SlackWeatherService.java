3a993a3d0abe27e13c67f0d10febc08f64f0f6cd
Seung-ik <snb4301@gmail.com>
2019-01-08 23:32:52 +0900

    change scheduled cron

--- src/main/java/com/ik/slackWeatherNotice/service/SlackWeatherService.java
+++ src/main/java/com/ik/slackWeatherNotice/service/SlackWeatherService.java
@@ -1,13 +1,12 @@
 package com.ik.slackWeatherNotice.service;
 
 
-import java.util.HashMap;
-import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -17,40 +16,37 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.ik.slackWeatherNotice.dto.SendSlackDTO;
 import com.ik.slackWeatherNotice.dto.WeatherDTO;
 
-
+@Slf4j
 @Service
 public class SlackWeatherService {
 	@Value("${openWeather.api.key}")
 	private String openWeatherKey;
-	
+
 	@Value("${openWeather.api.uri}")
 	private String openWeatherUri;
-	
+
 	@Value("${slack.webHooks.uri}")
 	private String slackWebHooksUri;
-	
+
 	@Autowired
 	RestTemplate restTemplate;
-	
-	public WeatherDTO getWeather() {
+
+	@Scheduled(cron="0 0/5 * * * *")
+	public void getWeather() {
 		WeatherDTO weatherDTO;
 		UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(openWeatherUri)
-									.queryParam("q", "Seongnam")
-									.queryParam("units", "metric")
-									.queryParam("appid", openWeatherKey)
-									.build();
-		System.out.println("###");
-		System.out.println("@@@ :  "+uriBuilder.toString());
-		
+				.queryParam("q", "Seoul")
+				.queryParam("units", "metric")
+				.queryParam("appid", openWeatherKey)
+				.build();
 		ResponseEntity<WeatherDTO> result = restTemplate.getForEntity(uriBuilder.toString(), WeatherDTO.class);
 		weatherDTO = result.getBody();
-		
+
 		SendSlackDTO sendSlackDTO = new SendSlackDTO();
-		sendSlackDTO.setText(String.format("[오늘의 날씨]\n 기온:%s\n 기압:%s\n 습도:%s\n 최고기온:%s\n 최저기온:%s\n",weatherDTO.getMainWeather().getTemp()
+		sendSlackDTO.setText(String.format("[오늘의 %s 날씨]\n 기온:%s\n 기압:%s\n 습도:%s\n 최고기온:%s\n 최저기온:%s\n","서울",weatherDTO.getMainWeather().getTemp()
 				,weatherDTO.getMainWeather().getPressure(),weatherDTO.getMainWeather().getHumidity(),weatherDTO.getMainWeather().getTemp_max(),
 				weatherDTO.getMainWeather().getTemp_min() ));
 
-		System.out.println("^^^"+sendSlackDTO.getText());
 		try {
 			restTemplate.postForObject(slackWebHooksUri, sendSlackDTO, String.class);
 
@@ -59,7 +55,5 @@ public class SlackWeatherService {
 		}catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
-		return weatherDTO;
 	}
 }
